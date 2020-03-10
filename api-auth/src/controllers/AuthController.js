228dda8631f6d82fe2b8aca514bbf3680286c803
Amadeusz Pecherczyk <a.pecherczyk@protonmail.com>
2020-03-10 22:42:34 +0100

    AuthController.getAuthRedirect divided to couple methods.

--- api-auth/src/controllers/AuthController.js
+++ api-auth/src/controllers/AuthController.js
@@ -32,37 +32,41 @@ class AuthController {
     if (!req.query.code) throw createError(400);
 
     const requestToken = req.query.code;
-    // accessToken declared out of arrow scope below to be available in Promise chain.
-    let accessToken;
 
     Promise.resolve()
-      .then(() => axios({
-        method: 'post',
-        url: 'https://github.com/login/oauth/access_token',
-        params: {
-          client_id: this._config.api.github.clientId,
-          client_secret: this._config.api.github.clientSecret,
-          code: requestToken
-        },
-        headers: {
-          accept: 'application/json'
-        }
-      }))
-      .then(response => response.data.access_token)
-      .then(token => {
-        accessToken = token;
-        return axios({
-          method: 'put',
-          url: this._config.api.putTokenUrl,
-          params: {
-            client_secret: this._config.api.github.clientSecret,
-            token: accessToken
-          }
-        })
-       })
-      .then(() => res.send(accessToken))
+      .then(() => this._getAccessToken(requestToken))
+      .then(response => this._putAccessToken(response.data.access_token))
+      .then(accessToken => res.send(accessToken))
       .then(next)
       .catch(next);
+  }
+
+  _getAccessToken (requestToken) {
+    return axios({
+      method: 'post',
+      url: 'https://github.com/login/oauth/access_token',
+      params: {
+        client_id: this._config.api.github.clientId,
+        client_secret: this._config.api.github.clientSecret,
+        code: requestToken
+      },
+      headers: {
+        accept: 'application/json'
+      }
+    });
+  }
+
+  _putAccessToken (accessToken) {
+    return Promise.resolve()
+      .then(() => axios({
+        method: 'put',
+        url: this._config.api.putTokenUrl,
+        params: {
+          client_secret: this._config.api.github.clientSecret,
+          token: accessToken
+        }
+      }))
+      .then(() => accessToken);
   }
 
   _getBaseUrl (req) {
